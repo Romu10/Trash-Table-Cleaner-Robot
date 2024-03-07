aa09a530e67158425a4444d4ae0348d47390c6d8
Romu10 <romulobryanp@gmail.com>
2024-03-07 16:51:44 +0000

    edit simple commander API script

--- nav2_interface/nav2_interface/look_for_trash_table.py
+++ nav2_interface/nav2_interface/look_for_trash_table.py
@@ -31,17 +31,6 @@ robot_init_position = {                                                     #
     "start_position": [0.00, 0.00, 0.00, 0.99]}                             #
 #############################################################################
 
-def goToPosition(navigator, location, position, frame = 'map'):
-    table_item_pose = PoseStamped()
-    table_item_pose.header.frame_id = frame
-    table_item_pose.header.stamp = navigator.get_clock().now().to_msg()
-    table_item_pose.pose.position.x = position[0]
-    table_item_pose.pose.position.y = position[1]
-    table_item_pose.pose.orientation.z = position[2]
-    table_item_pose.pose.orientation.w = position[3]
-    print('Looking for trash table picking at ' + location + '.')
-    navigator.goToPose(table_item_pose)
-
 def setRobotInitPosition(navigator, location, position, frame = 'map'):
     initial_pose = PoseStamped()
     initial_pose.header.frame_id = 'map'
@@ -53,15 +42,46 @@ def setRobotInitPosition(navigator, location, position, frame = 'map'):
     print('Set initial position at ' + location + '.')
     navigator.setInitialPose(initial_pose)
 
+def goToPosition(navigator, location, position, frame = 'map'):
+    table_item_pose = PoseStamped()
+    table_item_pose.header.frame_id = frame
+    table_item_pose.header.stamp = navigator.get_clock().now().to_msg()
+    table_item_pose.pose.position.x = position[0]
+    table_item_pose.pose.position.y = position[1]
+    table_item_pose.pose.orientation.z = position[2]
+    table_item_pose.pose.orientation.w = position[3]
+    print('Looking for trash table picking at ' + location + '.')
+    navigator.goToPose(table_item_pose)
+
+def arrivalTime(navigator, request_table_location):
+    i = 0
+    while not navigator.isTaskComplete():
+        i = i + 1
+        feedback = navigator.getFeedback()
+        if feedback and i % 5 == 0:
+            print('Estimated time of arrival at ' + request_table_location +
+                  ' for drop off: ' + '{0:.0f}'.format(
+                      Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                  + ' seconds.')
+
+def getTaskResult(navigator, request_table_location):
+    result = navigator.getResult()
+    if result == TaskResult.SUCCEEDED:
+        print('Looking for trash table in ' + request_table_location + ' .')
+
+    elif result == TaskResult.CANCELED:
+        print('Task at ' + request_table_location + ' was canceled. Returning to staging point...')\
+
+        # Return to starter position
+        goToPosition(navigator, 'Start Position', robot_init_position['start_position'])
+
+    elif result == TaskResult.FAILED:
+        print('Task at ' + request_table_location + ' failed!')
+        exit(-1)
+
 def main():
 
-    ####################
-    request_item_location = 'position_1'
-    request_destination = 'backroom_2'
-    ####################
-
     rclpy.init()
-
     navigator = BasicNavigator()
 
     # Set your demo's initial pose
@@ -70,34 +90,30 @@ def main():
     # Wait for navigation to activate fully
     navigator.waitUntilNav2Active()
 
+    # Define trash table final destination
+    request_destination = 'backroom_2'
+
     # Go to position 1
-    goToPosition(navigator, 'Position 1', table_trash_positions['position_1'])
+    request_table_location = 'Position 1'
+    goToPosition(navigator, request_table_location, table_trash_positions['position_1'])
+ 
+    # Arrival Time 
+    arrivalTime(navigator, request_table_location)
 
-    i = 0
-    while not navigator.isTaskComplete():
-        i = i + 1
-        feedback = navigator.getFeedback()
-        if feedback and i % 5 == 0:
-            print('Estimated time of arrival at ' + request_item_location +
-                  ' for drop off: ' + '{0:.0f}'.format(
-                      Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
-                  + ' seconds.')
+    # Get Task Result 
+    getTaskResult(navigator, request_table_location)
 
-    result = navigator.getResult()
-    if result == TaskResult.SUCCEEDED:
-        print('Got trash table from ' + request_item_location + '! Bringing trash table to shipping destination: ' + request_destination + '...')
-        
-        # Go to position backroom 2
-        goToPosition(navigator, 'Backroom', shipping_destinations['backroom_2'], 'map')
+    # Go to position 2
+    request_table_location = 'Position 2'
+    goToPosition(navigator, request_table_location, table_trash_positions['position_2'])
+ 
+    # Arrival Time 
+    arrivalTime(navigator, request_table_location)
 
-    elif result == TaskResult.CANCELED:
-        print('Task at ' + request_item_location + ' was canceled. Returning to staging point...')
-        goToPosition(navigator, 'Start Position', robot_init_position['start_position'])
+    # Get Task Result 
+    getTaskResult(navigator, request_table_location)
 
 
-    elif result == TaskResult.FAILED:
-        print('Task at ' + request_item_location + ' failed!')
-        exit(-1)
 
     while not navigator.isTaskComplete():
         pass
